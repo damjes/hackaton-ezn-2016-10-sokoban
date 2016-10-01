1a760b41a34051a921d685af55f822aea1cb4f7f
matma6 <matma6@matma6.net>
2016-10-01 13:03:23 +0200

    Gra gotowa

--- sokoban.rb
+++ sokoban.rb
@@ -1,15 +1,26 @@
 require 'gosu'
 
 class Stan
+  attr_reader :brakuje
   def initialize
     @plansza = [
-      [:x, :x, :x, :x, :x, :x, :x],
-      [:x, :v, :p, :s, :p, :c, :x],
-      [:x, :x, :x, :x, :x, :x, :x]
+      [:p, :p, :p, :p, :p, :p, :p, :p, :x, :x, :x, :x, :x, :p, :p, :p, :p],
+      [:p, :p, :p, :p, :p, :p, :p, :p, :x, :p, :p, :p, :x, :x, :x, :x, :x],
+      [:p, :p, :p, :p, :p, :p, :p, :p, :x, :p, :x, :s, :x, :x, :p, :p, :x],
+      [:p, :p, :p, :p, :p, :p, :p, :p, :x, :p, :p, :p, :p, :p, :s, :p, :x],
+      [:x, :x, :x, :x, :x, :x, :x, :x, :x, :p, :x, :x, :x, :p, :p, :p, :x],
+      [:x, :c, :c, :c, :c, :p, :p, :x, :x, :p, :s, :p, :p, :s, :x, :x, :x],
+      [:x, :c, :c, :c, :c, :p, :p, :p, :p, :s, :p, :s, :s, :p, :x, :x, :p],
+      [:x, :c, :c, :c, :c, :p, :p, :x, :x, :s, :p, :p, :s, :p, :p, :x, :p],
+      [:x, :x, :x, :x, :x, :x, :x, :x, :x, :p, :p, :s, :p, :p, :x, :x, :p],
+      [:p, :p, :p, :p, :p, :p, :p, :p, :x, :p, :s, :p, :s, :p, :p, :x, :p],
+      [:p, :p, :p, :p, :p, :p, :p, :p, :x, :x, :x, :p, :p, :p, :p, :x, :p],
+      [:p, :p, :p, :p, :p, :p, :p, :p, :p, :p, :x, :p, :p, :p, :p, :x, :p],
+      [:p, :p, :p, :p, :p, :p, :p, :p, :p, :p, :x, :x, :x, :x, :x, :x, :p],
     ]
-    @wymiary = [3, 7]
-    @gracz = [2, 1]
-    @obrazki = Gosu::Image::load_tiles("grafiki.png", 128, 128)
+    @gracz = [14, 7]
+    @brakuje = 12
+    @obrazki = Gosu::Image::load_tiles("grafiki.png", 32, 32)
   end
 
   def narysuj
@@ -44,7 +55,32 @@ class Stan
             -1
           end
         end
-        @obrazki[num].draw 128*x, 128*y, 0 if num != -1
+        @obrazki[num].draw 32*x, 32*y, 0 if num != -1
+      end
+    end
+  end
+
+  def probuj_przesunac deltax, deltay
+    plus_1 = @plansza[@gracz[1] + deltay][@gracz[0] + deltax]
+    if plus_1 == :p or plus_1 == :c
+      @gracz = [@gracz[0] + deltax, @gracz[1] + deltay]
+    end
+    if plus_1 == :s or plus_1 == :v
+      plus_2 = @plansza[@gracz[1] + 2*deltay][@gracz[0] + 2*deltax]
+      if plus_2 == :p or plus_2 == :c
+        if plus_1 == :s
+          @plansza[@gracz[1] + deltay][@gracz[0] + deltax] = :p
+        else
+          @plansza[@gracz[1] + deltay][@gracz[0] + deltax] = :c
+          @brakuje += 1
+        end
+        if plus_2 == :p
+          @plansza[@gracz[1] + 2*deltay][@gracz[0] + 2*deltax] = :s
+        else
+          @plansza[@gracz[1] + 2*deltay][@gracz[0] + 2*deltax] = :v
+          @brakuje -= 1
+        end
+        @gracz = [@gracz[0] + deltax, @gracz[1] + deltay]
       end
     end
   end
@@ -55,13 +91,40 @@ class GameWindow < Gosu::Window
     super 1024,768
     self.caption = "Sokoban"
     @stan = Stan.new
+    @blokada = 0
+    @font = Gosu::Font.new(200)
   end
 
   def update
+    close if Gosu::button_down? Gosu::KbEscape
+    if @blokada > 0 or @stan.brakuje == 0
+      @blokada -= 1
+    else
+      if Gosu::button_down? Gosu::KbLeft
+        @stan.probuj_przesunac -1, 0
+        @blokada = 10
+      end
+      if Gosu::button_down? Gosu::KbRight
+        @stan.probuj_przesunac 1, 0
+        @blokada = 10
+      end
+      if Gosu::button_down? Gosu::KbDown
+        @stan.probuj_przesunac 0, 1
+        @blokada = 10
+      end
+      if Gosu::button_down? Gosu::KbUp
+        @stan.probuj_przesunac 0, -1
+        @blokada = 10
+      end
+    end
   end
 
   def draw
-    @stan.narysuj
+    if @stan.brakuje == 0
+      @font.draw("Wygrana", 12, 12, 3, 1, 1, Gosu::Color::WHITE)
+    else
+      @stan.narysuj
+    end
   end
 end
 
