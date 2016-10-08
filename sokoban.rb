fb482b21794f3b8731f883734f92c0b1f93582cf
matma6 (inĝ. Mikaelo Gabor) <matma6@matma6.net>
2016-10-08 02:56:10 +0200

    Teraz plansza wczytuje się z pliku PPM

--- sokoban.rb
+++ sokoban.rb
@@ -3,6 +3,7 @@ require 'gosu'
 class Stan
 	attr_reader :brakuje
 	def initialize
+=begin
 		@plansza = [
 			[:p, :p, :p, :p, :p, :p, :p, :p, :x, :x, :x, :x, :x, :p, :p, :p, :p],
 			[:p, :p, :p, :p, :p, :p, :p, :p, :x, :p, :p, :p, :x, :x, :x, :x, :x],
@@ -20,6 +21,9 @@ class Stan
 		]
 		@gracz = [14, 7]
 		@brakuje = 12
+=end
+		@brakuje = 0
+		czytaj_plik 'plansza.ppm'
 		@obrazki = Gosu::Image::load_tiles("grafiki.png", 32, 32)
 		@przes = Gosu::Sample.new("dzwieki/przes.wav")
 		@zalicz = Gosu::Sample.new("dzwieki/zalicz.wav")
@@ -96,6 +100,90 @@ class Stan
 		end
 	end
 
+	def czytaj_kolor strumien
+		r = strumien.getbyte
+		g = strumien.getbyte
+		b = strumien.getbyte
+
+		if r == 0
+			if g == 0
+				if b == 0
+					# czarny
+					return :p
+				elsif b == 255
+					# niebieski
+					return :gracz
+				end
+			elsif g == 255
+				if b == 0
+					# zielony
+					return :s
+				end
+			end
+		elsif r == 255
+			# czerwona składowa to pole docelowe
+			if g == 0
+				if b == 0
+					# czerwony
+					return :c
+				elsif b == 255
+					# fioletowy
+					# niebieski na czerwonym inaczej mówiąc
+					return :gracz_na_c
+				end
+			elsif g == 255
+				if b == 0
+					# żółty
+					# zielony na czerwonym
+					return :v
+				elsif b == 255
+					# biały
+					return :x
+				end
+			end
+		end
+
+		raise 'Nieznany kolor'
+	end
+
+	def przetworz_kolor strumien, x, y
+		kolor = czytaj_kolor strumien
+		if kolor == :gracz
+			@gracz = [x, y]
+			return :p
+		elsif kolor == :gracz_na_c
+			@gracz = [x, y]
+			return :c
+		else
+			return kolor
+		end
+	end
+
+	def czytaj_macierz strumien, x, y
+		@plansza = []
+		y.times do |nr_wiersza|
+			wiersz = []
+			x.times do |nr_kolumny|
+				symbol = przetworz_kolor strumien, nr_kolumny, nr_wiersza
+				wiersz << symbol
+				@brakuje += 1 if symbol == :s
+			end
+			@plansza << wiersz
+		end
+	end
+
+	def czytaj_plik plik
+		File.open(plik, 'r') do |uchwyt|
+			raise 'Wymagany tryb P6' unless uchwyt.gets.chomp == 'P6'
+			linia = uchwyt.gets
+			while linia.chr == '#'
+				linia = uchwyt.gets
+			end
+			wymiary = linia.split
+			raise 'Wymagana paleta 255 wartości na kanał' unless uchwyt.gets.chomp == '255'
+			czytaj_macierz uchwyt, wymiary[0].to_i, wymiary[1].to_i
+		end
+	end
 end
 
 class GameWindow < Gosu::Window
